1e280b644058eb67a983665c007cbd1bda518180
Housni Mohamed <mohamed.housni@um5s.net.ma>
2023-12-01 22:59:30 +0100

    index commited

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.
+// React is a free and open-source front-end JavaScript library for building user interfaces based on UI components
 import React from 'react';
 
 // ReactDOM is a package that provides DOM specific methods that can be used at the top level of a web app to enable an efficient way of managing DOM elements of the web page.
 import ReactDOM from 'react-dom';
 
-// import component
+// import component 2
 import App from './App';
 
 // render the component at the page root
